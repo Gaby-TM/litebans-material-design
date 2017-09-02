be9008e4c25dd553cf4c058fefb1cc058bb011b0
darbyjack <admin@glaremasters.me>
2017-09-02 01:00:26 -0500

    Initial Commit

---
+++ error/unconfigured.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>litebans-php - Unconfigured</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/css/mdb.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="jumbotron">
+        <h1>Unconfigured</h1><br>
+
+        <h3>            Welcome to the LiteBans web interface!
+            <br>
+            To get started, edit <a class="text-warning">inc/settings.php</a> and fill in your database information.
+            <br>
+            After configuration has been completed, click <a class="text-warning">"Continue"</a>.<h3>
+            <br>
+        
+        <br>
+        <a href="../" class="btn btn-cyan">Continue</a>
+    </div>
+</div>
+</body>
+</html>
