3a6f4e1a64f6aef8052f7d42477dcf98f88128cb
darbyjack <admin@glaremasters.me>
2018-04-19 15:54:43 -0500

    Changed to font awesome icons

--- inc/page.php
+++ inc/page.php
@@ -432,8 +432,8 @@ class Page {
             }
             $a = "a";
             if ($header === $this->t("warnings.received")) {
-                $icon = ($text !== "0") ? "glyphicon-ok" : "glyphicon-remove";
-                $a .= " class=\"glyphicon $icon\" aria-hidden=true";
+                $icon = ($text !== "0") ? "fas fa-check fa-2x" : "fas fa-times fa-2x";
+                $a .= " class=\"$icon\" aria-hidden=true";
                 $text = "";
             }
             echo "<td $style><$a href=\"info.php?type=$type&id=$id\">$text</a></td>";
